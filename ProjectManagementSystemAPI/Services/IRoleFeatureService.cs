d9f936c0198169692fad9a25a36bc76ae986d6b4
Ziad Saleh <76475951+Ziad-d@users.noreply.github.com>
2024-09-18 16:33:07 +0300

    Refactor HasAccess with CQRS query
    - Removed the HasAccess service method and replaced it with a CQRS query.
    - Created CheckRoleFeatureAccessQuery to encapsulate role and feature parameters.
    - Implemented CheckRoleFeatureAccessQueryHandler to handle query logic and repository access.

--- ProjectManagementSystemAPI/Services/IRoleFeatureService.cs
+++
@@ -1,9 +0,0 @@
-﻿using ProjectManagementSystemAPI.Constants.Enum;
-
-namespace ProjectManagementSystemAPI.Services
-{
-    public interface IRoleFeatureService
-    {
-        bool HasAccess(int roleID, Feature feature);
-    }
-}
