2f181b757dfa77d10a6e5042ce9cb06685d2b651
Ziad Saleh <76475951+Ziad-d@users.noreply.github.com>
2024-09-18 19:40:34 +0300

    Renaming DTO to DTOS

---
+++ ProjectManagementSystemAPI/DTOs/UserDTOs/UserDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace ProjectManagementSystemAPI.DTO.Users
+{
+    public class UserDTO
+    {
+        public int Id { get; set; }
+        public string UserName { get; set; }
+        public int RoleId { get; set; }
+    }
+}
