a7f8de99288f750daefd36728b20fbf6a4311acc
Ziad Saleh <76475951+Ziad-d@users.noreply.github.com>
2024-09-18 19:30:25 +0300

    Removed JWT Config from app.settings
    - Remove unnecessary JWT Configurations from app.settings.
    - Created a static class to hold the data.

---
+++ ProjectManagementSystemAPI/Constants/JwtSettings.cs
@@ -0,0 +1,10 @@
+﻿namespace ProjectManagementSystemAPI.Constants
+{
+    public static class JwtSettings
+    {
+        public static string Key { get; set; } = "ABYREQ$EWIEOUSLHT#@!WPIDTREFRSEE*&^%DHGFDREE";
+        public static string Issuer { get; set; } = "UpSkilling";
+        public static string Audience { get; set; } = "UpSkilling-Users";
+        public static int DurationInMinutes { get; set; } = 60;
+    }
+}
