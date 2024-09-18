c2eb5f8334eb64c9cf19df699602c8f414352ba6
Ziad Saleh <76475951+Ziad-d@users.noreply.github.com>
2024-09-18 19:39:47 +0300

    Update context

--- ProjectManagementSystemAPI/Data/Context.cs
+++ ProjectManagementSystemAPI/Data/Context.cs
@@ -5,10 +5,6 @@ namespace ProjectManagementSystemAPI.Data
 {
     public class Context : DbContext 
     {
-        public Context( )  
-        {
-
-        }
         public Context(DbContextOptions options) : base(options)
         {
             ChangeTracker.QueryTrackingBehavior = QueryTrackingBehavior.NoTracking;
@@ -16,6 +12,11 @@ namespace ProjectManagementSystemAPI.Data
 
         public DbSet<User> Users { get; set; }
         public DbSet<Role> Roles { get; set; }
+        public DbSet<Project> Projects { get; set; }
+        public DbSet<Model.Task> Tasks { get; set; }
+        public DbSet<ProjectTask> ProjectTasks { get; set; }
+        public DbSet<UserTask> UserTasks { get; set; }
+        public DbSet<UserProject> UserProjects { get; set; }
         public DbSet<UserRole> UserRoles { get; set; }        
     }
 }
