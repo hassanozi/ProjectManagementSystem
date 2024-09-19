6be86a5da529d899c870c00ba188da6ad72d8500
Ziad Saleh <76475951+Ziad-d@users.noreply.github.com>
2024-09-19 22:00:40 +0300

    Update Context

--- ProjectManagementSystemAPI/Data/Context.cs
+++ ProjectManagementSystemAPI/Data/Context.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using ProjectManagementSystemAPI.Model;
+using System.Diagnostics;
+using System.Reflection;
 
 namespace ProjectManagementSystemAPI.Data
 {
@@ -10,11 +12,21 @@ namespace ProjectManagementSystemAPI.Data
             ChangeTracker.QueryTrackingBehavior = QueryTrackingBehavior.NoTracking;
         }
 
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+
+            foreach (var relationship in modelBuilder.Model.GetEntityTypes().SelectMany(e => e.GetForeignKeys()))
+            {
+                relationship.DeleteBehavior = DeleteBehavior.Restrict;
+            }
+        }
+
         public DbSet<User> Users { get; set; }
         public DbSet<Role> Roles { get; set; }
         public DbSet<Project> Projects { get; set; }
         public DbSet<Model.Task> Tasks { get; set; }
-        public DbSet<ProjectTask> ProjectTasks { get; set; }
+        public DbSet<RoleFeature> RoleFeatures { get; set; }
         public DbSet<UserTask> UserTasks { get; set; }
         public DbSet<UserProject> UserProjects { get; set; }
         public DbSet<UserRole> UserRoles { get; set; }        
