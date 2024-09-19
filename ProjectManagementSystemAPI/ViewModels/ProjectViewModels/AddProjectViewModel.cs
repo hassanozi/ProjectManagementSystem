945f8fc42754b17473c53df4149b05fc99fd879c
Ziad Saleh <76475951+Ziad-d@users.noreply.github.com>
2024-09-19 22:30:57 +0300

    Add ViewModels

---
+++ ProjectManagementSystemAPI/ViewModels/ProjectViewModels/AddProjectViewModel.cs
@@ -0,0 +1,11 @@
+﻿using ProjectManagementSystemAPI.Enums;
+
+namespace ProjectManagementSystemAPI.ViewModels.ProjectViewModels
+{
+    public class AddProjectViewModel
+    {
+        public string Title { get; set; }
+        public string? Description { get; set; }
+        public StatusProject Status { get; set; }
+    }
+}
---
+++ ProjectManagementSystemAPI/ViewModels/ProjectViewModels/ProjectViewModel.cs
@@ -0,0 +1,15 @@
+﻿using ProjectManagementSystemAPI.Enums;
+using ProjectManagementSystemAPI.Model;
+
+namespace ProjectManagementSystemAPI.ViewModels.ProjectViewModels
+{
+    public class ProjectViewModel
+    {
+        public string Title { get; set; }
+        public string? Description { get; set; }
+        public DateTime CreatedOn { get; set; } = DateTime.Now;
+        public StatusProject Status { get; set; }
+        //public List<Model.Task> Tasks { get; set; }
+        //public List<User> Users { get; set; }
+    }
+}
