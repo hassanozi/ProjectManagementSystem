cbdb6a288b7d3a90517aa8c8e4e3770a1dfa5845
Ziad Saleh <76475951+Ziad-d@users.noreply.github.com>
2024-09-19 22:26:57 +0300

    Exception Handling

---
+++ ProjectManagementSystemAPI/Enums/ErrorCode.cs
@@ -0,0 +1,16 @@
+﻿namespace ProjectManagementSystemAPI.Enums
+{
+    public enum ErrorCode
+    {
+        None = 0,
+        UnKnown = 1,
+
+        UserNotFound = 1000,
+
+        ProjectNotFound = 2000,
+        NotValidProjectID = 2001,
+        NotValidProjectData = 2002,
+
+        NotValidDates = 2002
+    }
+}
--- ProjectManagementSystemAPI/Middlewares/GlobalErrorHandlerMiddleware.cs
+++ ProjectManagementSystemAPI/Middlewares/GlobalErrorHandlerMiddleware.cs
@@ -1,4 +1,8 @@
-﻿namespace ProjectManagementSystemAPI.Middlewares
+﻿using ProjectManagementSystemAPI.Enums;
+using ProjectManagementSystemAPI.Exceptions;
+using ProjectManagementSystemAPI.ViewModels;
+
+namespace ProjectManagementSystemAPI.Middlewares
 {
     public class GlobalErrorHandlerMiddleware
     {
@@ -18,22 +22,21 @@
             catch (Exception ex)
             {
                 string message = "Error Occured";
-                //ErrorCode errorCode = ErrorCode.UnKnown;
+                ErrorCode errorCode = ErrorCode.UnKnown;
 
-                //if (ex is BusinessException businessException)
-                //{
-                //    message = businessException.Message;
-                //    errorCode = businessException.ErrorCode;
-                //}
-                //else
-                //{
-                File.WriteAllText("D:\\Log.txt", $"Error happened: {ex.Message}");
-                throw;
-                //}
+                if (ex is BusinessException businessException)
+                {
+                    message = businessException.Message;
+                    errorCode = businessException.ErrorCode;
+                }
+                else
+                {
+                    File.WriteAllText("D:\\Log.txt", $"Error happened: {ex.Message}");
+                }
 
-                //var result = ResultViewModel<bool>.Faliure(errorCode, message);
+                var result = ResponseViewModel<bool>.Faliure(errorCode, message);
 
-                //await context.Response.WriteAsJsonAsync(result);
+                await context.Response.WriteAsJsonAsync(result);
             }
         }
     }
