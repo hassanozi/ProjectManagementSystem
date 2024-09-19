9f2b18e65839bc62b9059e6750af83be3503c23b
Ziad Saleh <76475951+Ziad-d@users.noreply.github.com>
2024-09-19 22:28:13 +0300

    Create ProjectProfile

---
+++ ProjectManagementSystemAPI/MapperProfile/ProjectProfile.cs
@@ -0,0 +1,23 @@
+﻿using AutoMapper;
+using ProjectManagementSystemAPI.DTOs.ProjectDTOs;
+using ProjectManagementSystemAPI.Model;
+using ProjectManagementSystemAPI.ViewModels.ProjectViewModels;
+
+namespace ProjectManagementSystemAPI.MapperProfile
+{
+    public class ProjectProfile : Profile
+    {
+        public ProjectProfile()
+        {
+            CreateMap<AddProjectDTO, Project>()
+                        .ForMember(dest => dest.Id, opt => opt.Ignore())
+                        .ForMember(dest => dest.CreatedOn, opt => opt.Ignore())
+                        .ForMember(dest => dest.Tasks, opt => opt.Ignore())
+                        .ForMember(dest => dest.UserProjects, opt => opt.Ignore());
+            CreateMap<AddProjectViewModel, AddProjectDTO>();
+
+            CreateMap<Project, ProjectDTO>().ReverseMap();
+            CreateMap<ProjectDTO, ProjectViewModel>().ReverseMap();
+        }
+    }
+}
