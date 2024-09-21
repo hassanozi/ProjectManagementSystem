6703fc091a14c9717f2875d2a812f4575f3d0f5b
Hassan Abdelrazek <hassan.abdelrazek.hussien@outlook.com>
2024-09-21 15:12:08 +0300

    TaskQuery

--- ProjectManagementSystemAPI/Controllers/TaskController.cs
+++ ProjectManagementSystemAPI/Controllers/TaskController.cs
@@ -2,8 +2,10 @@
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using ProjectManagementSystemAPI.CQRS.Tasks.Commands;
+using ProjectManagementSystemAPI.CQRS.Tasks.Queries;
 using ProjectManagementSystemAPI.DTOs.TaskDTOs;
 using ProjectManagementSystemAPI.ViewModels;
+using ProjectManagementSystemAPI.ViewModels.TaskViewModels;
 
 namespace ProjectManagementSystemAPI.Controllers
 {
@@ -31,5 +33,13 @@ namespace ProjectManagementSystemAPI.Controllers
             return Ok(result);
         }
 
+
+        [HttpGet]
+        public async Task<IEnumerable<TaskViewModel>> GetAllTasks()
+        {
+            var result = await _mediator.Send(new GetTasksListQuery());
+
+            return (IEnumerable<TaskViewModel>)result;
+        }
     }
 }
---
+++ ProjectManagementSystemAPI/ViewModels/TaskViewModels/TaskViewModel.cs
@@ -0,0 +1,13 @@
+﻿using ProjectManagementSystemAPI.Enum;
+
+namespace ProjectManagementSystemAPI.ViewModels.TaskViewModels
+{
+    public class TaskViewModel
+    {
+        public string Title { get; set; }
+        public StatusTask Status { get; set; }
+        public int User { get; set; }
+        public int Project { get; set; }
+        public DateTime DateCreated { get; set; }= DateTime.Now;
+    }
+}
